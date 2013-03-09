2c227b96e96c194da311345bf4f4723134e9bb8c
James <jrm.general@gmail.com>
2013-03-09 00:43:47 -0500

    Finished FSM demo

--- design_patterns/FSM.js
+++ design_patterns/FSM.js
@@ -1,5 +1,6 @@
 // Uses jQuery's event API to trigger and bind events on the FSM
 
+var $ = require('jquery');
 
 (function(){
   "use strict";
@@ -25,4 +26,41 @@
       this.o.trigger.apply(this.o, arguments);
     }
   };
+
+  var StateMachine = function(){};
+  StateMachine.fn = StateMachine.prototype;
+
+  $.extend(StateMachine.fn, Events); // Add event/binding, triggering
+
+  // Add the passed controller to the list of states
+  StateMachine.fn.add = function(controller){
+    this.bind("change", function(e, current){
+      if(controller === current){
+        controller.activate();
+      }else{
+        controller.deactivate();
+      }
+    });
+
+    // Modifies the passed controller, providing for state transitions
+    controller.active = $.proxy(function(){
+      this.trigger("change", controller);
+    }, this);
+  };
+
+  var con1 = {
+    activate: function(){ console.log("con1 activated");},
+    deactivate: function(){ console.log("con1 deactivated");}
+  }, 
+  con2 = {
+    activate: function(){ console.log("con2 activated");},
+    deactivate: function(){ console.log("con2 deactivated");}
+  };
+
+  var sm = new StateMachine();
+  sm.add(con1);
+  sm.add(con2);
+
+  con1.active();
+
 }());
